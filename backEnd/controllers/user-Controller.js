8ec4f89cf8e1150d952b67116817005b49c7fbf7
erkaerka21 <eegiibaagii597@gmail.com>
2024-08-30 05:43:53 +0800

    830

--- backEnd/controllers/user-Controller.js
+++ backEnd/controllers/user-Controller.js
@@ -10,18 +10,23 @@ const getUsers = async (req, res) => {
   res.status(200).json({ message: "GET success", user: dataUser });
 };
 const postUser = async (req, res) => {
+  const { email, name, password, avatar_img } = req.body;
   const postUser =
-    await sqlUser`INSERT INTO users(email, name, password, avatar_img) VALUES('naraa@yahoo.com','Naran','14223333d','https://www.parliament.mn/files/589971694cd748a582718307ca063ce9?d=0');`;
+    await sqlUser`INSERT INTO users(email, name, password, avatar_img) VALUES(${email},${name},${password},${avatar_img});`;
   // const data = fs.readFileSync("./users.json", { encoding: "utf8" });
   // const userData = JSON.parse(data);
-  // console.log("POST harah ajillaj bga eseh : ", userData);
+  console.log("POST harah ajillaj bga eseh : ", postUser);
   res.status(201).json({ message: "post amjilttai", user: postUser });
 };
 const editUser = async (req, res) => {
+  const { id } = req.params;
+  const { email, name, password, avatar_img } = req.body;
+  const editUser =
+    await sqlUser`UPDATE users SET email=${email},name=${name},password=${password},avatar_img=${avatar_img} WHERE id=${id};`;
   // const data = fs.readFileSync("./users.json", { encoding: "utf8" });
   // const userData = JSON.parse(data);
-  // console.log("PUT harah ajillaj bga eseh : ", userData);
-  // res.status(400).json({ message: "not found" });
+  console.log("EDIT harah ajillaj bga eseh : ", editUser);
+  res.status(200).json({ message: "edit amjilttai", user: editUser });
 };
 const deleteUser = async (req, res) => {
   // const data = fs.readFileSync("./users.json", { encoding: "utf8" });
